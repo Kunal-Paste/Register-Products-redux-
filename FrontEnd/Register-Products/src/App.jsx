bbd6e633900576adeb3eda3b91b8d9b4fbc9958b
Kunal-Paste <pastekunal6@gamail.com>
2025-06-20 15:05:37 +0530

    current user and login logout

--- FrontEnd/Register-Products/src/App.jsx
+++ FrontEnd/Register-Products/src/App.jsx
@@ -2,21 +2,27 @@ import React, { useEffect } from 'react'
 import NavBar from './Components/NavBar'
 import MainRoute from './Routes/MainRoute'
 import axios from './Api/axiosconfig'
+import { useDispatch } from 'react-redux'
+import { asynccurrentuser } from './Store/Actions/UserAction'
 
 const App = () => {
-  const getproducts = async () => {
-    try {
-      let res = await axios.get("/products")
-      console.log(res);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(asynccurrentuser());
+  },[])
+  // const getproducts = async () => {
+  //   try {
+  //     let res = await axios.get("/products")
+  //     console.log(res);
 
-    } catch (error) {
-      console.log(error);
+  //   } catch (error) {
+  //     console.log(error);
 
-    }
-  }
-  useEffect(() => {
-    getproducts();
-  }, [])
+  //   }
+  // }
+  // useEffect(() => {
+  //   getproducts();
+  // }, [])
   return (
     <>
       <NavBar />
--- FrontEnd/Register-Products/src/Store/Actions/UserAction.jsx
+++ FrontEnd/Register-Products/src/Store/Actions/UserAction.jsx
@@ -1,22 +1,34 @@
 import axios from "../../Api/axiosconfig";
+import { loaduser } from "../Reducers/UserSlice";
 
-export const asyncloginuser = (user) => async (dispatch,getState)=>{
+export const asynccurrentuser = () => async (dispatch, getState) => {
    try {
-    const {data} = await axios.get(`/users?password=${user.password}`);
-    localStorage.setItem("users",JSON.stringify(data[0]));
+      const user = JSON.parse(localStorage.getItem("users"));
+      if (user) dispatch(loaduser(user));
+      else console.log('user not logged in');
+      
    } catch (error) {
-    console.log(error);
+      console.log(error);
    }
 }
 
-export const asyncregisteruser = (user) => async (dispatch,getState)=>{
+export const asyncloginuser = (user) => async (dispatch, getState) => {
    try {
-    const res = await axios.post("/users",user);
-    console.log(res);
-    
+      const { data } = await axios.get(`/users?password=${user.password}`);
+      localStorage.setItem("users", JSON.stringify(data[0]));
    } catch (error) {
-    console.log(error); 
-    // updates had been made ...
-    
+      console.log(error);
+   }
+}
+
+export const asyncregisteruser = (user) => async (dispatch, getState) => {
+   try {
+      const res = await axios.post("/users", user);
+      console.log(res);
+
+   } catch (error) {
+      console.log(error);
+      // updates had been made ...
+
    }
 }
